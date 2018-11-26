e8c669f9eed6d6ea59b918eda90821b9eb676e47
Paula Kuwałek <paula.kuwalek@gmail.com>
2018-11-26 15:48:39 +0100

    add masonry-like gallery grid and ES6 features

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
+app.get("/", (req, res) => {
     res.render("landing");
 });
 
-app.get("/home", function(req,res){
+app.get("/home", (req, res) => {
     var photos = [
         {image: "imgs/candles.jpg"},
         {image: "imgs/spiders.jpg"},
+        {image: "imgs/black-cat.jpg"},
+        {image: "imgs/ingredients.jpg"},
+        {image: "imgs/book.jpg"},
+        {image: "imgs/cookies.jpg"},
+        {image: "imgs/jack-o-lantern.jpg"},
     ]
     res.render("home", {photos:photos});
 });
 
-app.get("/login", function(req,res){
+app.get("/login", (req, res) => {
     res.render("login");
 });
 
-app.get("/register", function(req,res){
+app.get("/register", (req, res) => {
     res.render("register");
 });
 
-app.listen(3000, function(){
+app.listen(3000, () => {
     console.log("The Server Has Started!");
  });
  
