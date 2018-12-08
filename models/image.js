39646aaee7bcc2dcb92765b508f4a155d54245ed
Paula Kuwałek <paula.kuwalek@gmail.com>
2018-12-08 21:01:35 +0100

    add image likes functionality

--- models/image.js
+++ models/image.js
@@ -3,10 +3,6 @@ const mongoose = require ("mongoose");
 var ImageSchema = new mongoose.Schema({
     path: String,
     title: String,
-    id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Image"
-    }
 });
 
 module.exports = mongoose.model("Image", ImageSchema);
--- models/user.js
+++ models/user.js
@@ -1,11 +1,35 @@
 const mongoose = require ("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
+var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var UserSchema = new mongoose.Schema({
     firstName: String,
     username: String,
-    password: String
+    password: String,
+    imgsLiked: [ObjectId]
 });
+
+//check if image was liked
+UserSchema.methods.findFavImage = function findFavImage (id) {
+    return this.imgsLiked.indexOf(id);
+};
+
+//if image was liked, delete & if not add to favourites
+UserSchema.methods.setFavImage = function setFavImage (id) {
+    var imageIndex = this.findFavImage(id)
+    if (imageIndex == -1){
+        this.imgsLiked.push(id);
+        this.save((err) => {
+            console.log(err);
+        });
+    }
+    else {
+        delete this.imgsLiked[imageIndex];
+        this.save((err) => {
+            console.log(err);
+        });
+    }
+};
 
 UserSchema.plugin(passportLocalMongoose);
 
